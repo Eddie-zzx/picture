b6834c513dbe6002340927e74e2bb7ff6722f0c7
zhengzexin <930571405@qq.com>
2026-01-12 15:50:42 +0800

    fix:去除会员制度

--- src/main/java/com/graduation/picture/constant/UserConstant.java
+++ src/main/java/com/graduation/picture/constant/UserConstant.java
@@ -16,11 +16,6 @@ public interface UserConstant {
     String DEFAULT_ROLE = "user";
 
     /**
-     * vip
-     */
-    String VIP_ROLE = "vip";
-
-    /**
      * 管理员角色
      */
     String ADMIN_ROLE = "admin";
--- src/main/java/com/graduation/picture/service/impl/UserServiceImpl.java
+++ src/main/java/com/graduation/picture/service/impl/UserServiceImpl.java
@@ -98,10 +98,10 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
             throw new BusinessException(ErrorCode.PARAMS_ERROR, "参数为空");
         }
         if (userAccount.length() < 4) {
-            throw new BusinessException(ErrorCode.PARAMS_ERROR, "账号错误");
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "用户不存在或密码错误");
         }
         if (userPassword.length() < 8) {
-            throw new BusinessException(ErrorCode.PARAMS_ERROR, "密码错误");
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "用户不存在或密码错误");
         }
         // 2. 加密
         String encryptPassword = getEncryptPassword(userPassword);
